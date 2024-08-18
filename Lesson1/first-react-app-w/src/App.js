32e2c30d57cff76396ea81ea73ded3bdb69c4aae
Khusein Muslim <khusein1993@gmail.com>
2024-08-18 14:32:07 +0300

    first-react-app

---
+++ Lesson1/first-react-app-w/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Message from './Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message name = {'Muslim'}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lesson1/first-react-app-w/src/Message.js
@@ -0,0 +1,9 @@
+function Message (props) {
+    return (
+        <h1>
+            Hello, i'm {props.name}, i'm 30 years old
+        </h1>
+    )
+}
+
+export default Message;
